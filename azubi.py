f2232b3cfdf70eab931ed468c4428ca297238881
PrincesssCharming <chantalwilkister@gmail.com>
2024-01-05 18:10:47 +0300

    tips

--- azubi.py
+++ azubi.py
@@ -1,19 +1,27 @@
 #print("Hello, world!")
-#name = input("what's your name?")
-#age = input("How old are you?")
+#name = input("what's your name? ")
+#age = input("How old are you? ")
 #print("Nice to meet you, " + age, " years old, " + name) 
-#need to add comma after the first variable(age) to avoid errors
+#need to add comma after the first variable(age) 
+#to avoid errors ie when writing variable name 
+#and message
 
-#name = input("Who are you? ")
-#print("Hello, " + name)
+#print("Hello " + name + " " + age)
 
-y = 10
+#y = 10
 #print(type(y))
 #finds out the type of the variable
 #can also be type(value)
 
-print(str(y) + " KES")
+#print(str(y) + " KES")
 #writing str changes the y variable to a string(changes number to text)
-x = float(input("Enter an amount: "))
+#x = float(input("Enter an amount: "))
 #float converts user input into a floating point number
-print(int(x))
+#print(int(x))
+
+sentence = "The cat is pretty"
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('e'))
+#self explanatory commands
