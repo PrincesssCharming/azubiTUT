2194ba9212d08af2841b27e14ba9f1710d3a8a00
PrincesssCharming <chantalwilkister@gmail.com>
2024-01-13 20:22:43 +0300

    week2

--- asankacalc.py
+++ asankacalc.py
@@ -9,7 +9,6 @@ tax = 7/100 * float(charge) #calculates sales tax
 #print(tax)
 tax_dollars = "${:.2f}".format(tax)
 print("Sales tax = " + str(tax_dollars))
-total_cost 
 
 total_cost = float(charge) + float(tip) + float(tax) #adds up the total bill
 cost_dollars = "${:.2f}".format(total_cost)
