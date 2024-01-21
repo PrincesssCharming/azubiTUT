5e0a9d3765dc4b7e966bbfd5cd83b5acba4dc6a6
PrincesssCharming <chantalwilkister@gmail.com>
2024-01-21 22:14:34 +0300

    week 3

---
+++ storesalesreview.py
@@ -0,0 +1,50 @@
+# Azubi store has products that customers love. Below are the products, prices and the number of customers that purchased products last week.
+# The owner wants you to do some calculations on the data with these criteria's:
+#     -calculate the total price average for all products
+#     -create a new price list that reduces the old prices by $ 5
+#     -calculate the total revenue generated from the products
+#     -calculate the average daily revenue generated from the products
+#     -based on the new prices, which products are less than $ 30 
+# Below is the data you are to use for the problem :
+# products = ["Sankofa Foods", "Jamestown Coffee", "Bioko Chocolate", "Blue Skies Ice Cream", "Fair Afric Chocolate", "Kawa Moka Coffee", "Aphro Spirit", "Mensado Bissap", "Voltic"]
+# prices = [30, 25, 40, 20, 20, 35, 45, 50, 35]
+# last_week = [2, 3, 5, 8, 4, 4, 6, 2, 9]
+
+products = ["Sankofa Foods", "Jamestown Coffee", "Bioko Chocolate", "Blue Skies Ice Cream", "Fair Afric Chocolate", "Kawa Moka Coffee", "Aphro Spirit", "Mensado Bissap", "Voltic"]
+prices = [30, 25, 40, 20, 20, 35, 45, 50, 35]
+last_week = [2, 3, 5, 8, 4, 4, 6, 2, 9]
+
+#calculate the total price average for all products
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers)
+
+total_price_avg = calculate_average(prices)
+print("The total price average for all products is ${}".format(round(total_price_avg)))
+
+#create a new price list that reduces the old prices by $ 5
+new_prices = [x-5 for x in prices]
+print("\nNew Prices List:\n{}".format(new_prices))
+
+#calculate the total revenue generated from the products
+def calculate_revenue(prices, quantities):
+    assert len(prices) == len(quantities), "There must be an equal number of prices and quantities"
+    return sum([p*q for p, q in zip(prices, quantities)])
+
+total_revenue = calculate_revenue(prices, last_week)
+print("\nTotal Revenue Generated:\n${}".format(round(total_revenue)))
+
+#calculate the average daily revenue generated from the products
+daily_revenue = round(total_revenue/sum(last_week))
+print("Average Daily Revenue Generated:\n${}".format(daily_revenue))
+
+#based on the new prices, which products are less than $ 30?
+less_than_30 = [product < 30 for product in new_prices]
+
+
+# daily_revenue = lambda day: day * calculate_average(last_week)
+# total_revenue = sum([day * price for day, price in zip(last_week, prices)])
+# daily_rev_avg = calculate_average([daily_revenue(i) for i in range(len(last_week))])
+# less_than_30 = [product < 30 for product in new_prices]
+# print("Total Revenue Generated: ", total_revenue)
+# print("Average Daily Rev Gen: ", daily_rev_avg)
+print("Products Less Than $30: ", less_than_30)
--- tut.py
+++ tut.py
@@ -42,13 +42,46 @@ print(names)'''
 
 #tuples
 '''colors = ("red", "green", "blue")
-print(colors[1])'''
+print(colors[1])
+#n/b we can have tuples inside tuples
 
-#dictionaries
-'''person = {'name': 'John', 'age': 30, 'city': 'Nairobi'}
-person['profession'] = 'Doctor' #an easy way to add to the list, the end of the list
+name = input("First name: ")
+surname = input("Surname: ")
+phone = input("Phone number: ")
+age = input("Age: ")
+
+employee = (name, surname, phone, age)
+
+print(employee)
+
+#len(tuple) - get the number of elements in a tuple
+#tuple.count(x) - number of elements in the tuple with value x
+#tuple.index(x) - index of the first element with value x, if x doesn't exist, an error will be shown
+
+numbers = (1, 2, "trois", "four", "V", 6)
+print(numbers[2:4]) #slicing
+print(len(numbers))
+print(numbers.count(1))
+print(numbers.index("V"))'''
+
+#dictionaries - used for student data, students and student IDs
+'''person = {'name': 'John', 'age': 30, 'city': 'Nairobi'} #key:value 9tuple can be used  as a key, lists can't
+person['profession'] = 'Doctor' #an easy way to add to the dictionary, the end of the list | modify content
 print(person)
-print(person['name'], person['age']) #print specific detail'''
+print(person['name'], person['age']) #print specific detail
+
+german_colours = {'red': 'rot', 'green': 'grun', 'blue': 'blau', 'yellow': 'gelb'}
+print(german_colours)
+
+eng_color = input("Name another English colour: ")
+germ_translation = input("Please enter the german translation of " + eng_color + ": ")
+
+german_colours[eng_color] = germ_translation
+print(german_colours)
+
+user_input = input("Which colour do you want translated? ")
+if user_input in german_colours:
+    print("The German translation for ", user_input, "is ", german_colours[user_input])'''
 
 #dictionaries and lists
 '''mary = {}
@@ -102,17 +135,123 @@ p3.remove("green") #removes any specified item
 p3.pop() #removes the last item on the list
 p1= tuple(p3)
 del p3
-print(p1)'''
+print(p1)
+
+#looping through tuples
+#for loop can be used to access individual elements of a tuple
+address = (52066, "Aachen", "Eupener Str. 70", "0241-6009-12345")
+
+for address_part in address:
+    print(address_part)'''
+
 
 #DICITONARIES
 
 
 #if...else statements
-name = input("Who are you? ")
+'''name = input("Who are you? ")
 
 if name == "Faith":
     print(f"Hello {name}, welcome!")
 elif name == "Jane":
     print(f"Hello {name}, enjoy!")
 else:
-    print("Access denied")
+    print("Access denied")'''
+
+#loops
+'''i = 1
+while i <=5 :
+    print(i)
+    if i == 3:
+        break #stops the while loop
+    i += 1
+
+#loops and conditions make up a program
+
+storedEmail = 'name@gmail.com'
+storedPass = '12345'
+
+failedLogIn = 0
+
+while failedLogIn < 3:
+    email = input("Enter your email: ")
+    passw = input("Enter your password: ")
+
+    failedLogIn += 1
+    remainingattempts = failedLogIn + 1
+
+    if email == storedEmail and passw == storedPass:
+        print("Login successful")
+    
+    else:
+        print(f"Incorrect email or Password. {remainingattempts} attempts left")#find out what's causing the error, fix it!!
+
+else:        
+    print("You have exceeded the maximum number of attempts.")
+    print("Program terminated.")
+
+#import json
+names = ["chantal", "ayobami", "regina"]
+
+selectedNames = []
+
+for name in names:
+    namess = selectedNames.append(name)
+
+    if name == "Regina":
+        break
+
+    print(selectedNames)'''
+
+#a student makes gonour roll if their average is >=85
+#and their lowest grade is not below 70
+
+'''gpa = float(input("What was your Grade Point Average? ")) #nesting functions, turns the 'str' input to a float
+lowest_grade = float(input("What was your lowest grade? "))
+
+if gpa >= .85 and lowest_grade >= .70:
+    print("Congratulations, you made the honor roll!")
+else:
+    print("Sorry, you did not make the honor roll this time around.")
+
+#if you need to check if student's on honour roll, you can use boolean
+if gpa >= .85 and lowest_grade >= .70:
+    honour_roll = True
+else:
+    honour_roll = False
+    print(honour_roll)'''
+
+#for loop
+'''for name in ['Christopher', 'Sam']:
+    print(name)'''
+
+# range loops a number of times
+'''for index in range(0, 2): #prints from 0 up to and nto including 2; 0,1
+    #for i in range(5): #prints 5 objects - 0,1,2,3,4
+    #for i in range(20, 30, 2): #start, stop+1, step size (from 20 until (not inclluding) 30, step size of 2)
+    #for i in range(30, 20, -1): #prints backwards
+    print(index)'''
+
+#while
+'''names = ['Jane', 'Kate']
+index = 0
+while index < len(names):
+    print(names[index])
+    index +=1 #must change condition in a while loop'''
+
+#modules and packages
+#module-python file with functions, classes and other components
+#package-folder that contains multiple modules. Used for organizing code
+#packages - published collections of modules, can be found on the internet with a search
+#the best code is borrowed code
+'''Importing a module
+1. import module as namespace
+    import helpers #module name is helpers.py
+    helpers.display('Not a warning') #display is the function name
+2. import all into current namespace
+    from helpers import *
+    display('Not a warning')
+3. import specific items into current namespace
+    from helpers import display
+    display('Not a warning)'''
+
